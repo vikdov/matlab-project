179a544bdc68306a6333f2921397cb0f2d59d190
Viktor <158165237+vikdov@users.noreply.github.com>
2026-04-14 15:19:31 +0200

    Enhance comments and documentation in hamilton.m

--- wspolne/hamilton.m
+++ wspolne/hamilton.m
@@ -1,23 +1,39 @@
 function mandaty = hamilton(glosy, liczba_mandatow)
 % HAMILTON  Metoda Hamiltona (największych reszt / metoda Hare'a)
-%   Kwota Hare'a = suma_głosów / liczba_mandatów
-%   glosy           - wektor głosów na partie
-%   liczba_mandatow - liczba mandatów do podziału
+% glosy           - wektor liczby głosów dla każdej partii
+% liczba_mandatow - liczba mandatów do podziału
 
+% Suma wszystkich głosów
 suma = sum(glosy);
+
+% Obliczamy kwotę Hare’a (ile głosów przypada na 1 mandat)
 kwota = suma / liczba_mandatow;
 
+% Obliczamy "udziały mandatowe" (ile mandatów powinna mieć partia idealnie)
 udzialy = glosy / kwota;
+
+% Bierzemy część całkowitą (to są mandaty przydzielone na pewno)
 mandaty_bazowe = floor(udzialy);
+
+% Reszty (czyli "niedobór" do kolejnego mandatu)
 reszty = udzialy - mandaty_bazowe;
 
+% Liczba mandatów już przydzielonych
 przydzielone = sum(mandaty_bazowe);
+
+% Ile mandatów jeszcze zostało do rozdania
 pozostale = liczba_mandatow - przydzielone;
 
-% Przydziel pozostałe mandaty partiom z największymi resztami
+% Sortujemy partie według reszt (malejąco)
+% order = indeksy partii od największej reszty
 [~, order] = sort(reszty, 'descend');
+
+% Na start mandaty = mandaty bazowe
 mandaty = mandaty_bazowe;
+
+% Przydzielamy pozostałe mandaty partiom z największymi resztami
 for k = 1:pozostale
     mandaty(order(k)) = mandaty(order(k)) + 1;
 end
+
 end
